b713d13893beafc4cf1a648c15c60e547888725e
sanjay jat <sanjayjatsanjay22@gmail.com>
2021-12-15 17:19:54 +0530

    added routing to app.js next is to modify Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
 
 export default class Navbar extends Component {
   render() {
@@ -6,9 +7,9 @@ export default class Navbar extends Component {
       <>
         <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
           <div className="container-fluid">
-            <a className="navbar-brand" href="/">
+            <Link className="navbar-brand" to="/">
               My News
-            </a>
+            </Link>
             <button
               className="navbar-toggler"
               type="button"
@@ -26,50 +27,50 @@ export default class Navbar extends Component {
             >
               <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                 <li className="nav-item">
-                  <a className="nav-link active" aria-current="page" href="/Home">
+                  <Link
+                    className="nav-link active"
+                    aria-current="page"
+                    to="/Home"
+                  >
                     Home
-                  </a>
+                  </Link>
                 </li>
 
                 <li className="nav-item">
-                  <a className="nav-link active" href="/business">
+                  <Link className="nav-link active" to="/business">
                     business
-                  </a>
+                  </Link>
                 </li>
                 <li className="nav-item">
-                  <a className="nav-link active" href="/entertainment">
+                  <Link className="nav-link active" to="/entertainment">
                     entertainment
-                  </a>
+                  </Link>
                 </li>
+
                 <li className="nav-item">
-                  <a className="nav-link active" href="/general">
-                    general
-                  </a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link active" href="/health">
+                  <Link className="nav-link active" to="/health">
                     health
-                  </a>
+                  </Link>
                 </li>
                 <li className="nav-item">
-                  <a className="nav-link active" href="/science">
+                  <Link className="nav-link active" to="/science">
                     science
-                  </a>
+                  </Link>
                 </li>
                 <li className="nav-item">
-                  <a className="nav-link active" href="/sports">
+                  <Link className="nav-link active" to="/sports">
                     sports
-                  </a>
+                  </Link>
                 </li>
                 <li className="nav-item">
-                  <a className="nav-link active" href="/technology">
+                  <Link className="nav-link active" to="/technology">
                     technology
-                  </a>
+                  </Link>
                 </li>
                 <li className="nav-item">
-                  <a className="nav-link active" href="/Contact">
+                  <Link className="nav-link active" to="/Contact">
                     Contact
-                  </a>
+                  </Link>
                 </li>
               </ul>
             </div>
